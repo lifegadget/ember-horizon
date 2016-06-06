cc76d934e2752efed191b348c36e96441c8f3837
Ken Snyder <ken@ken.net>
2016-06-06 09:52:04 +0100

    fixes blueprint problem

--- blueprints/ember-cli-horizon/index.js
+++ blueprints/ember-cli-horizon/index.js
@@ -1,13 +1,15 @@
 /*jshint node:true*/
 module.exports = {
     description: 'Default blueprint to be run after install.',
-
+    normalizeEntityName: function() {
+		// this prevents an error when the entityName is not specified
+	},
     afterInstall: function() {
         return this.addPackagesToProject([
-            { package: '@horizon/client' },
-            { package: 'bufferutil' },
-            { package: 'utf-8-validate' },
-            { package: 'ember-browserify' }
+            { name: '@horizon/client', target: '^1.1.1' },
+            { name: 'bufferutil', target: '^1.2.1'  },
+            { name: 'utf-8-validate', target: '^1.2.1' },
+            { name: 'ember-browserify', target: '^1.1.9' }
         ]);
     }
 };
