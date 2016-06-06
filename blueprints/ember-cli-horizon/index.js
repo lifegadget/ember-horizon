f796b96d5c74b5015817b0dcacef7fe04bb7b4bd
Ken Snyder <ken@ken.net>
2016-06-06 10:06:11 +0100

    starting service refactor

--- blueprints/ember-cli-horizon/index.js
+++ blueprints/ember-cli-horizon/index.js
@@ -1,15 +1,15 @@
 /*jshint node:true*/
 module.exports = {
-    description: 'Default blueprint to be run after install.',
-    normalizeEntityName: function() {
-		// this prevents an error when the entityName is not specified
+  description: 'Default blueprint to be run after install.',
+  normalizeEntityName: function() {
+    // this prevents an error when the entityName is not specified
 	},
-    afterInstall: function() {
-        return this.addPackagesToProject([
-            { name: '@horizon/client', target: '^1.1.1' },
-            { name: 'bufferutil', target: '^1.2.1'  },
-            { name: 'utf-8-validate', target: '^1.2.1' },
-            { name: 'ember-browserify', target: '^1.1.9' }
-        ]);
-    }
+  afterInstall: function() {
+    return this.addPackagesToProject([
+      { name: '@horizon/client', target: '^1.1.1' },
+      { name: 'bufferutil', target: '^1.2.1'  },
+      { name: 'utf-8-validate', target: '^1.2.1' },
+      { name: 'ember-browserify', target: '^1.1.9' }
+    ]);
+  }
 };
