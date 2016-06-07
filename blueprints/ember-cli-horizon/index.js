bae6d5e4309d295a3999373c84f26b4f22435a3b
Ken Snyder <ken@ken.net>
2016-06-07 15:00:26 +0100

    remove deps from afterInstall hook

--- blueprints/ember-cli-horizon/index.js
+++ blueprints/ember-cli-horizon/index.js
@@ -9,25 +9,16 @@ module.exports = {
 	},
   afterInstall: function() {
     var self = this;
-    return new RSVP.Promise(function(resolve, reject) {
+    return new RSVP.Promise(function(resolve) {
 
-      self.addPackagesToProject([
-          { name: '@horizon/client', target: '^1.1.1' },
-          { name: 'bufferutil', target: '^1.2.1'  },
-          { name: 'utf-8-validate', target: '^1.2.1' },
-          { name: 'ember-browserify', target: '^1.1.9' }
-      ])
-        .then(function() {
-          console.log(chalk.green.bold('ember-cli-horizon') + ' has been installed along with all client dependencies.');
-          console.log('  - in order to use this you will need to install the Horizon server,');
-          console.log('    you can find more information here: ' + chalk.blue.underline('http://horizon.io/install/'));
-          console.log();
-          console.log('In development mode you can serve both Horizon server and Ember\'s "serve" by');
-          console.log('using the ' + chalk.bold('ember horizon:serve') + ' command.');
-          console.log();
-          resolve(true);
-        })
-        .catch(reject);
+      console.log(chalk.green.bold('ember-cli-horizon') + ' has been installed along with all client dependencies.');
+      console.log('  - in order to use this you will need to install the Horizon server,');
+      console.log('    you can find more information here: ' + chalk.blue.underline('http://horizon.io/install/'));
+      console.log();
+      console.log('In development mode you can serve both Horizon server and Ember\'s "serve" by');
+      console.log('using the ' + chalk.bold('ember horizon:serve') + ' command.');
+      console.log();
+      resolve();
 
     });
   },
