6d8d8517fd64d9531ff5e42d4bce8b90e7579754
Kyle Jones <kyle@jonesetc.com>
2016-06-06 20:40:45 -0500

    fix tabbing

--- addon/adapters/horizon.js
+++ addon/adapters/horizon.js
@@ -12,149 +12,149 @@ const { RSVP: {Promise}, get, inject: {service}, typeOf } = Ember;
  * many, or all collections.
  */
 export default Adapter.extend({
-    horizon: service(),
+  horizon: service(),
 
-    // Horizon returns objects as plain JSON.
-    defaultSerializer: 'json',
+  // Horizon returns objects as plain JSON.
+  defaultSerializer: 'json',
 
-    findRecord(store, type, id) {
-      const horizon = get(this, 'horizon');
-      const subscription = horizon.getSubscription(type);
-      return new Promise((resolve, reject) => {
+  findRecord(store, type, id) {
+    const horizon = get(this, 'horizon');
+    const subscription = horizon.getSubscription(type);
+    return new Promise((resolve, reject) => {
 
-        // If subscription exists we assume record already up-to-date
-        if(subscription) {
-          resolve( this.peekRecord(type.modelName, id) );
-        } else {
-          horizon.collection(type)
-            .then(c => horizon.find(c, id))
-            .then(horizon.fetch)
-            .then(horizon.subscribe)
-            .then(resolve)
-            .catch(reject);
-        }
-
-      }); // return promise
-    },
-
-    findAll(store, type) {
-      const horizon = get(this, 'horizon');
-      const subscription = horizon.getSubscription(type);
-      return new Promise((resolve, reject) => {
-
-        // If subscription exists we assume record already up-to-date
-        if(subscription) {
-          resolve( this.peekAll(type.modelName) );
-        } else {
-          horizon.collection(type)
-            .then(horizon.findAll)
-            .then(horizon.fetch)
-            .then(horizon.subscribe)
-            .then(resolve)
-            .catch(reject);
-        }
-
-      }); // return promise
-    },
-
-    findMany(store, type, ids) {
-      const horizon = get(this, 'horizon');
-      const subscription = horizon.getSubscription(type);
-      return new Promise((resolve, reject) => {
-
-        // If subscription exists we assume record already up-to-date
-        if(subscription) {
-          resolve( this.peekAll(type.modelName, ids) );
-        } else {
-          horizon.collection(type)
-            .then(c => horizon.findAll(c, ids))
-            .then(horizon.fetch)
-            .then(horizon.subscribe)
-            .then(resolve)
-            .catch(reject);
-        }
-
-      }); // return promise
-    },
-
-    query(store, type, query) {
-      const horizon = get(this, 'horizon');
-      const subscription = horizon.getSubscription(type);
-      return new Promise((resolve, reject) => {
-
-        // If subscription exists we assume record already up-to-date
-        if(subscription) {
-          resolve( this.peekAll(type.modelName, query) );
-        } else {
-          horizon.collection(type)
-            .then(c => horizon.findAll(c, query))
-            .then(horizon.fetch)
-            .then(horizon.subscribe)
-            .then(resolve)
-            .catch(reject);
-        }
-
-      }); // return promise
-    },
-
-    queryRecord(store, type, query) {
-      const horizon = get(this, 'horizon');
-      const subscription = horizon.getSubscription(type);
-      return new Promise((resolve, reject) => {
-
-        // If subscription exists we assume record already up-to-date
-        if(subscription) {
-          resolve( this.peekAll(type.modelName, query) ); // TODO: this may not work with query
-        } else {
-          horizon.collection(type)
-            .then(c => horizon.find(c, query))
-            .then(horizon.fetch)
-            .then(horizon.subscribe)
-            .then(resolve)
-            .catch(reject);
-        }
-
-      }); // return promise
-    },
-
-    createRecord(store, type, snapshot) {
-      const horizon = get(this, 'horizon');
-      const payload = this.serialize(snapshot);
-      return new Promise((resolve, reject) => {
-
+      // If subscription exists we assume record already up-to-date
+      if(subscription) {
+        resolve( this.peekRecord(type.modelName, id) );
+      } else {
         horizon.collection(type)
-          .then(c => horizon.store(c, payload))
+          .then(c => horizon.find(c, id))
+          .then(horizon.fetch)
           .then(horizon.subscribe)
           .then(resolve)
           .catch(reject);
+      }
 
-      }); // return promise
-    },
+    }); // return promise
+  },
 
-    updateRecord(store, type, snapshot) {
-      const horizon = get(this, 'horizon');
-      const payload = this.serialize(snapshot, { includeId: true });
-      return new Promise((resolve, reject) => {
+  findAll(store, type) {
+    const horizon = get(this, 'horizon');
+    const subscription = horizon.getSubscription(type);
+    return new Promise((resolve, reject) => {
 
+      // If subscription exists we assume record already up-to-date
+      if(subscription) {
+        resolve( this.peekAll(type.modelName) );
+      } else {
         horizon.collection(type)
-          .then(c => horizon.replace(c, payload))
+          .then(horizon.findAll)
+          .then(horizon.fetch)
           .then(horizon.subscribe)
           .then(resolve)
           .catch(reject);
+      }
 
-      }); // return promise
-    },
+    }); // return promise
+  },
 
-    deleteRecord(store, type, snapshot) {
-      const horizon = get(this, 'horizon');
-      const id = typeOf(snapshot) === 'class' ? snapshot.id : snapshot;
-      return new Promise((resolve, reject) => {
+  findMany(store, type, ids) {
+    const horizon = get(this, 'horizon');
+    const subscription = horizon.getSubscription(type);
+    return new Promise((resolve, reject) => {
 
+      // If subscription exists we assume record already up-to-date
+      if(subscription) {
+        resolve( this.peekAll(type.modelName, ids) );
+      } else {
         horizon.collection(type)
-          .then(c => horizon.remove(c, id))
+          .then(c => horizon.findAll(c, ids))
+          .then(horizon.fetch)
+          .then(horizon.subscribe)
           .then(resolve)
           .catch(reject);
+      }
 
-      }); // return promise
-    },
+    }); // return promise
+  },
+
+  query(store, type, query) {
+    const horizon = get(this, 'horizon');
+    const subscription = horizon.getSubscription(type);
+    return new Promise((resolve, reject) => {
+
+      // If subscription exists we assume record already up-to-date
+      if(subscription) {
+        resolve( this.peekAll(type.modelName, query) );
+      } else {
+        horizon.collection(type)
+          .then(c => horizon.findAll(c, query))
+          .then(horizon.fetch)
+          .then(horizon.subscribe)
+          .then(resolve)
+          .catch(reject);
+      }
+
+    }); // return promise
+  },
+
+  queryRecord(store, type, query) {
+    const horizon = get(this, 'horizon');
+    const subscription = horizon.getSubscription(type);
+    return new Promise((resolve, reject) => {
+
+      // If subscription exists we assume record already up-to-date
+      if(subscription) {
+        resolve( this.peekAll(type.modelName, query) ); // TODO: this may not work with query
+      } else {
+        horizon.collection(type)
+          .then(c => horizon.find(c, query))
+          .then(horizon.fetch)
+          .then(horizon.subscribe)
+          .then(resolve)
+          .catch(reject);
+      }
+
+    }); // return promise
+  },
+
+  createRecord(store, type, snapshot) {
+    const horizon = get(this, 'horizon');
+    const payload = this.serialize(snapshot);
+    return new Promise((resolve, reject) => {
+
+      horizon.collection(type)
+        .then(c => horizon.store(c, payload))
+        .then(horizon.subscribe)
+        .then(resolve)
+        .catch(reject);
+
+    }); // return promise
+  },
+
+  updateRecord(store, type, snapshot) {
+    const horizon = get(this, 'horizon');
+    const payload = this.serialize(snapshot, { includeId: true });
+    return new Promise((resolve, reject) => {
+
+      horizon.collection(type)
+        .then(c => horizon.replace(c, payload))
+        .then(horizon.subscribe)
+        .then(resolve)
+        .catch(reject);
+
+    }); // return promise
+  },
+
+  deleteRecord(store, type, snapshot) {
+    const horizon = get(this, 'horizon');
+    const id = typeOf(snapshot) === 'class' ? snapshot.id : snapshot;
+    return new Promise((resolve, reject) => {
+
+      horizon.collection(type)
+        .then(c => horizon.remove(c, id))
+        .then(resolve)
+        .catch(reject);
+
+    }); // return promise
+  },
 });
