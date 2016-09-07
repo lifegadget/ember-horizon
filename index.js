2954add67c4de1fe46464833d5afffdc987659c5
Ken Snyder <ken@ken.net>
2016-09-07 14:38:05 +0100

    provide config defaults

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 /* jshint node: true */
 'use strict';
-
+var _ = require('lodash');
 var commands = require('./lib/commands');
 
 module.exports = {
   name: 'ember-horizon',
-  includedCommands() {
+  includedCommands: function() {
     return commands;
   },
-  contentFor(type, config) {
+  contentFor: function(type, config) {
+    // Horizon Client Library
     if (type === 'head') {
-      var horizonConfig = config['horizon'];
+      var horizonConfig = Object.assign(_.get(config, 'horizon', {}), {
+        host: 'localhost:8181',
+        authType: 'anonymous'
+      });
       var protocol = horizonConfig.host.match(/(localhost|127.0.0.1)/) ? 'http://' : 'https://';
       return '<script id="client-driver" src="' + protocol + horizonConfig.host + '/horizon/horizon.js"></script>';
     }
